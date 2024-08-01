4707f489abb19afcdc059574aa27b7181bcf0bc7
exp-fool-coder <s.maslow98@gmail.com>
2024-08-01 18:18:15 +0400

    some quality of life improvements

--- src/main/java/com/expfool/Main.java
+++ src/main/java/com/expfool/Main.java
@@ -4,6 +4,7 @@ import com.expfool.controller.TicketStatisticController;
 import com.expfool.dao.TicketFileReader;
 import com.expfool.dto.api.TicketStatisticAnswer;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,7 +21,17 @@ public class Main {
         try {
             TicketStatisticAnswer ticketStatisticAnswer = ticketStatisticController.getSomeStatisticFromTicketsFile(path, "VVO", "TLV");
             System.out.println(ticketStatisticAnswer);
+            promptEnterKey();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void promptEnterKey(){
+        System.out.println("Press \"ENTER\" to exit...");
+        try {
+            int read = System.in.read(new byte[2]);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
